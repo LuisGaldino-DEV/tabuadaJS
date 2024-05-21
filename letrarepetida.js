f21a1fbbaf77f183801b6dc534d22df76ed5e8d4
Luis Galdino <luisgaldino.dev@gmail.com>
2024-05-21 08:51:56 -0300

    fix: correção bug loop

--- letrarepetida.js
+++ letrarepetida.js
@@ -2,7 +2,7 @@ const palavra = "Luis".toLowerCase().replaceAll(" ", "");
 let letras = {}
 
 //Contar quantoas letras tem em uma palavra
-//console.log(palavra.length )
+//console.log(palavra.length  )
 
 for (let i = 0; i < palavra.length; i++) {
   if (letras[palavra[i]]) {  
